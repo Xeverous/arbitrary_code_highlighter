00fea608dd8853d9ca8868d34bb06d2ed29952e0
Xeverous <20820409+Xeverous@users.noreply.github.com>
2022-11-19 18:42:40 +0100

    binding code for clangd highlighter

--- src/ach/clangd/highlighter_error.hpp
+++ src/ach/clangd/highlighter_error.hpp
@@ -4,6 +4,8 @@
 #include <ach/text/types.hpp>
 #include <ach/utility/range.hpp>
 
+#include <string_view>
+
 namespace ach::clangd {
 
 enum class error_reason
@@ -23,6 +25,42 @@ enum class error_reason
 	internal_error_token_to_action,
 	internal_error_unhandled_end_of_input
 };
+
+constexpr std::string_view to_string(error_reason reason)
+{
+	switch (reason) {
+		case error_reason::syntax_error:
+			return "syntax_error";
+		case error_reason::unsupported:
+			return "unsupported";
+		case error_reason::invalid_semantic_token_data:
+			return "invalid_semantic_token_data";
+		case error_reason::internal_error_missed_semantic_token:
+			return "internal_error_missed_semantic_token";
+		case error_reason::internal_error_unhandled_preprocessor:
+			return "internal_error_unhandled_preprocessor";
+		case error_reason::internal_error_unhandled_context:
+			return "internal_error_unhandled_context";
+		case error_reason::internal_error_unhandled_context_no_preprocessor:
+			return "internal_error_unhandled_context_no_preprocessor";
+		case error_reason::internal_error_unhandled_comment:
+			return "internal_error_unhandled_comment";
+		case error_reason::internal_error_raw_string_literal_quote_open:
+			return "internal_error_raw_string_literal_quote_open";
+		case error_reason::internal_error_raw_string_literal_quote_close:
+			return "internal_error_raw_string_literal_quote_close";
+		case error_reason::internal_error_raw_string_literal_paren_open:
+			return "internal_error_raw_string_literal_paren_open";
+		case error_reason::internal_error_raw_string_literal_paren_close:
+			return "internal_error_raw_string_literal_paren_close";
+		case error_reason::internal_error_token_to_action:
+			return "internal_error_token_to_action";
+		case error_reason::internal_error_unhandled_end_of_input:
+			return "internal_error_unhandled_end_of_input";
+	}
+
+	return "(unknown)";
+}
 
 struct highlighter_error
 {
--- src/python/binding.cpp
+++ src/python/binding.cpp
@@ -1,14 +1,19 @@
+#include "ach/clangd/highlighter_error.hpp"
 #include <ach/mirror/color_options.hpp>
 #include <ach/mirror/core.hpp>
+#include <ach/clangd/semantic_token.hpp>
+#include <ach/clangd/core.hpp>
 #include <ach/utility/version.hpp>
 #include <ach/utility/visitor.hpp>
 
 #include <pybind11/pybind11.h>
 
-#include <algorithm>
-#include <optional>
 #include <stdexcept>
 #include <sstream>
+#include <string>
+#include <string_view>
+#include <variant>
+#include <vector>
 
 namespace py = pybind11;
 
@@ -21,7 +26,7 @@ std::string to_string(const ach::mirror::highlighter_error& error)
 	return ss.str();
 }
 
-py::str run_highlighter(
+py::str run_mirror_highlighter(
 	// required arguments
 	std::string_view code, std::string_view color,
 	// optional, keyword arguments
@@ -69,12 +74,217 @@ py::str run_highlighter(
 	}, result);
 }
 
+ach::clangd::semantic_token_type parse_semantic_token_type(std::string_view name)
+{
+	using stt = ach::clangd::semantic_token_type;
+
+	if (name == "variable")
+		return stt::variable;
+	else if (name == "parameter")
+		return stt::parameter;
+	else if (name == "function")
+		return stt::function;
+	else if (name == "method")
+		return stt::method;
+	else if (name == "property")
+		return stt::property;
+	else if (name == "class")
+		return stt::class_;
+	else if (name == "interface")
+		return stt::interface;
+	else if (name == "enum")
+		return stt::enum_;
+	else if (name == "enumMember")
+		return stt::enum_member;
+	else if (name == "type")
+		return stt::type;
+	else if (name == "unknown")
+		return stt::unknown;
+	else if (name == "namespace")
+		return stt::namespace_;
+	else if (name == "typeParameter")
+		return stt::template_parameter;
+	else if (name == "concept")
+		return stt::concept_;
+	else if (name == "macro")
+		return stt::macro;
+	else if (name == "comment")
+		return stt::disabled_code;
+
+	throw py::value_error(std::string("unknown SemanticToken type: ").append(name).c_str());
+}
+
+std::vector<ach::clangd::semantic_token_type> parse_semantic_token_types(const py::list& list_semantic_token_types)
+{
+	std::vector<ach::clangd::semantic_token_type> result;
+	result.reserve(list_semantic_token_types.size());
+
+	for (const auto& token_type : list_semantic_token_types)
+		result.push_back(parse_semantic_token_type(token_type.cast<std::string_view>()));
+
+	return result;
+}
+
+using apply_token_mod_f = void (ach::clangd::semantic_token_modifiers&);
+apply_token_mod_f* parse_semantic_token_modifier(std::string_view name)
+{
+	using stm = ach::clangd::semantic_token_modifiers;
+	using stsm = ach::clangd::semantic_token_scope_modifier;
+
+	if (name == "declaration")
+		return +[](stm& m) { m.is_declaration = true; };
+	else if (name == "deprecated")
+		return +[](stm& m) { m.is_deprecated = true; };
+	else if (name == "deduced")
+		return +[](stm& m) { m.is_deduced = true; };
+	else if (name == "readonly")
+		return +[](stm& m) { m.is_readonly = true; };
+	else if (name == "static")
+		return +[](stm& m) { m.is_static = true; };
+	else if (name == "abstract")
+		return +[](stm& m) { m.is_abstract = true; };
+	else if (name == "virtual")
+		return +[](stm& m) { m.is_virtual = true; };
+	else if (name == "dependentName")
+		return +[](stm& m) { m.is_dependent_name = true; };
+	else if (name == "defaultLibrary")
+		return +[](stm& m) { m.is_from_std_lib = true; };
+	else if (name == "usedAsMutableReference")
+		return +[](stm& m) { m.is_out_parameter = true; };
+	else if (name == "functionScope")
+		return +[](stm& m) { m.scope = stsm::function; };
+	else if (name == "classScope")
+		return +[](stm& m) { m.scope = stsm::class_; };
+	else if (name == "fileScope")
+		return +[](stm& m) { m.scope = stsm::file; };
+	else if (name == "globalScope")
+		return +[](stm& m) { m.scope = stsm::global; };
+
+	throw py::value_error(std::string("unknown SemanticToken modifier: ").append(name).c_str());
+}
+
+std::vector<apply_token_mod_f*> parse_semantic_token_modifiers(const py::list& list_semantic_token_modifiers)
+{
+	std::vector<apply_token_mod_f*> result;
+	result.reserve(list_semantic_token_modifiers.size());
+
+	for (const auto& token_modifier : list_semantic_token_modifiers)
+		result.push_back(parse_semantic_token_modifier(token_modifier.cast<std::string_view>()));
+
+	return result;
+}
+
+ach::clangd::semantic_token_info get_semantic_token_info(
+	const std::vector<ach::clangd::semantic_token_type>& semantic_token_types,
+	const std::vector<apply_token_mod_f*>& semantic_token_modifiers,
+	const pybind11::handle& semantic_token)
+{
+	const auto token_type = semantic_token.attr("token_type").cast<std::size_t>();
+
+	if (token_type >= semantic_token_types.size()) {
+		throw py::value_error(("SemanticToken has token_type " + std::to_string(token_type) + " but only "
+			+ std::to_string(semantic_token_types.size()) + " token types were reported!").c_str());
+	}
+
+	const auto token_modifiers = semantic_token.attr("token_modifiers").cast<std::size_t>();
+
+	ach::clangd::semantic_token_modifiers mods;
+	for (std::size_t i = 0; i < semantic_token_modifiers.size(); ++i)
+		if (((1u << i) & token_modifiers) != 0)
+			semantic_token_modifiers[i](mods);
+
+	return ach::clangd::semantic_token_info{semantic_token_types[token_type], mods};
+}
+
+std::vector<std::string> parse_keywords(const py::list& list_keywords)
+{
+	std::vector<std::string> result;
+	result.reserve(list_keywords.size());
+
+	for (const auto& obj : list_keywords)
+		result.push_back(obj.cast<std::string>());
+
+	return result;
+}
+
+std::string to_string(const ach::clangd::highlighter_error& error)
+{
+	std::stringstream ss;
+	ss << "[line: " << error.pos.line << ", column: " << error.pos.column << "]"
+		" error: " << ach::clangd::to_string(error.reason) << "\n";
+
+	if (!error.last_semantic_tokens.empty())
+		ss << "last semantic tokens:\n";
+
+	for (ach::clangd::semantic_token token : error.last_semantic_tokens) {
+		ss << "\t[line: " << token.pos.line
+			<< ", column: " << token.pos.column
+			<< ", length" << token.length << "]\n";
+	}
+
+	return ss.str();
+}
+
+py::str run_clangd_highlighter(
+	std::string_view code,
+	const py::list& list_semantic_token_types,
+	const py::list& list_semantic_token_modifiers,
+	const py::list& list_semantic_tokens,
+	const py::list& list_keywords,
+	std::string_view table_wrap_css_class,
+	int color_variants)
+{
+	const std::vector<ach::clangd::semantic_token_type> semantic_token_types =
+		parse_semantic_token_types(list_semantic_token_types);
+
+	const std::vector<apply_token_mod_f*> semantic_token_modifiers =
+		parse_semantic_token_modifiers(list_semantic_token_modifiers);
+
+	const std::vector<std::string> keywords = parse_keywords(list_keywords);
+
+	std::vector<ach::clangd::semantic_token> semantic_tokens;
+	semantic_tokens.reserve(list_semantic_tokens.size());
+
+	for (const auto& token : list_semantic_tokens) {
+		semantic_tokens.push_back(ach::clangd::semantic_token{
+			ach::text::position{
+				token.attr("line").cast<std::size_t>(),
+				token.attr("column").cast<std::size_t>()
+			},
+			token.attr("length").cast<std::size_t>(),
+			get_semantic_token_info(semantic_token_types, semantic_token_modifiers, token),
+			ach::clangd::semantic_token_color_variance{
+				token.attr("color_variant").cast<int>(),
+				token.attr("last_reference").cast<bool>()
+			}
+		});
+	}
+
+	const std::variant<std::string, ach::clangd::highlighter_error> result = ach::clangd::run_highlighter(
+		code,
+		ach::utility::range<const ach::clangd::semantic_token*>{
+			semantic_tokens.data(), semantic_tokens.data() + semantic_tokens.size()},
+		ach::utility::range<const std::string*>{keywords.data(), keywords.data() + keywords.size()},
+		ach::clangd::highlighter_options{table_wrap_css_class, color_variants});
+
+	return std::visit(ach::utility::visitor{
+		[](const std::string& output) {
+			return py::str(output);
+		},
+		[](ach::clangd::highlighter_error error) -> py::str {
+			throw std::runtime_error(to_string(error));
+		}
+	}, result);
+}
+
 }
 
 PYBIND11_MODULE(pyach, m) {
 	m.doc() = ach::utility::program_description;
 
-	m.def("run_highlighter", &run_highlighter, py::arg("code").none(false), py::arg("color").none(false),
+	m.def("run_mirror_highlighter", &run_mirror_highlighter,
+		py::arg("code").none(false),
+		py::arg("color").none(false),
 		py::arg("num_class")        = ach::mirror::color_options::default_num_class,
 		py::arg("str_class")        = ach::mirror::color_options::default_str_class,
 		py::arg("str_esc_class")    = ach::mirror::color_options::default_str_esc_class,
@@ -85,6 +295,15 @@ PYBIND11_MODULE(pyach, m) {
 		py::arg("table_wrap_css_class") = "",
 		py::arg("valid_css_classes")    = "",
 		py::arg("replace") = false);
+
+	m.def("run_clangd_highlighter", &run_clangd_highlighter,
+		py::arg("code").none(false),
+		py::arg("semantic_token_types").none(false),
+		py::arg("semantic_token_modifiers").none(false),
+		py::arg("semantic_tokens").none(false),
+		py::arg("keywords").none(false),
+		py::arg("table_wrap_css_class") = "",
+		py::arg("color_variants") = ach::clangd::highlighter_options{}.color_variants);
 
 	m.def("version", []() {
 		namespace av = ach::utility::version;
